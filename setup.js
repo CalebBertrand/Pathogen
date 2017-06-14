82a3f66be6bb8098307728b070668fa2ecc1070b
CalebBertrand <caleb.a.bertrand@gmail.com>
2017-06-14 12:45:16 -0500

    Support for Chrome, Safari, and many more
    ;)

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-new p5();
+// new p5();
 // var backgroundImg;
 // function preload() {
 //   backgroundImg = loadImage("imgs/background.png");
@@ -8,7 +8,7 @@ var cells;
 var p1;
 function setup() {
 	var ctx = createCanvas(window.innerWidth, window.innerHeight);
-	ctx.parent('canvas-container');
+	// ctx.parent('canvas-container');
 	particles = createParticles();
 	p1 = new player(0, 0, 2.5, 25, 0.7);
 	cells = createCells();
--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 var maxBacteriumSize = 100;
 var maxWhiteBloodSize = 170;
-var perlin = random(0, 100);
+var perlin = /*random(0, 100)*/ 0;
 //the mutation variable starts at a random number. Certain mutations occur if the mutation variable is certain numbers
 var mutation = 0;
 //enter the number of the mutation and it returns the appropriate message
